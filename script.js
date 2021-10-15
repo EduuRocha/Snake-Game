bf149f660ac3473a8af530a0bd3b2c38fad56b4c
Eduardo <rochapaulaed@gmail.com>
2021-10-15 18:34:56 -0300

    This should work

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+let gameOverControl;
+let playAgain=document.getElementById('playAgain')
+let scores=document.getElementById('score');
+let winOrlose= document.getElementById('winOrlose');
 let board=document.getElementById('board')
 let snake=document.querySelectorAll('.snake')
-let row=5;
-let column=5;
-snake[0].style.gridRow=row;
-snake[0].style.gridColumn=column;
+
+snake[0].style.gridRow=5;
+snake[0].style.gridColumn=5;
 let counter=document.getElementById('counterApple')
 let counterMovements=document.getElementById('counterMovements')
 let movements=[{
-    row: snake[0].style.gridRow,
-    column: snake[0].style.gridColumn
+    row: 5,
+    column:5
 }];
 let snakeBody=[]
 
@@ -23,25 +26,16 @@ class Movement{
 
 
 let apple=document.getElementById('apple')
-let a;
+let randomAppleValue;
 generateApple()
 function generateApple(){
-    a= randomApple()
-    const [aRowCoordinate,aColumnCoordinate]=a
+    randomAppleValue= randomApple()
+    const [aRowCoordinate,aColumnCoordinate]=randomAppleValue
     apple.style.gridRow=aRowCoordinate
     apple.style.gridColumn=aColumnCoordinate
 }
-window.addEventListener('keydown',move)
 
-function move(e){
-    switch(e.key){
-        case 'ArrowUp':
-            if (row>1){
-                row--;
-                snake[0].style.gridRow=row
-                } 
-            break;
-
+<<<<<<< HEAD
         case 'ArrowDown':
             if (row<9){
                 row++;
@@ -73,27 +67,38 @@ function move(e){
     moveBody()
 
 }
+=======
+>>>>>>> cssBranch
 
 function randomApple(){
     let aRow=Math.ceil(Math.random()*10)
     let aColumn=Math.ceil(Math.random()*10);
-    if (aRow==row || aRow>9){
+    if (aRow==snake[0].style.gridRow || aRow>9){
        
       aRow= randomApple()[0]
     }
-    if (aColumn==column || aColumn>9){
+    if (aColumn==snake[0].style.gridColumn || aColumn>9){
       
         aColumn= randomApple()[1]
+    }
+
+    for(let i =0; i<snakeBody.length;i++){
+        if (aRow== snakeBody[i].style.gridRow && aColumn==snakeBody[i].style.gridColumn){
+            [aRow,aColumn]=randomApple()
+        }
     }
     return [aRow, aColumn]
 }
 
 function eatApple(x,y){
-    if (x[0]===y[0] && x[1]===y[1]){
+    if (x[0]==y[0] && x[1]==y[1]){
         generateApple()
-        counter.innerHTML++
+        counter.innerHTML--
         grow()
     }
+    if (counter.innerHTML==0){
+        return gameOver('WIN')
+    }   
 }
 
 
@@ -107,9 +112,127 @@ function grow(){
     snakeBody.push(el);
 }
 
+
+
 function moveBody(){
     for (let i=0; i<snakeBody.length;i++){
         snakeBody[i].style.gridColumn=movements[counterMovements.innerHTML-(i+1)].column
         snakeBody[i].style.gridRow=movements[counterMovements.innerHTML-(i+1)].row
+    }
+}
+
+
+setInterval(walk,400);
+
+
+let switchControl;
+window.addEventListener('keydown',(e)=>{
+    switchControl=e.key
+})
+function walk(){
+    if (gameOverControl){
+        return 
+    }
+   switch(switchControl){
+       case 'ArrowUp':
+           if( snake[0].style.gridRow>1){
+            snake[0].style.gridRow--
+           }
+           else{
+               return gameOver('You Lose')
+           }
+           break;
+
+        
+        case 'ArrowDown':
+            if (snake[0].style.gridRow<9){
+                snake[0].style.gridRow++
+            }
+            else{
+                return gameOver('You Lose')
+            }
+            break;
+        
+        case 'ArrowLeft':
+            if (snake[0].style.gridColumn>1){
+                snake[0].style.gridColumn--
+            }
+            else{
+                return gameOver('You Lose')
+            }
+            break;
+
+        case 'ArrowRight':
+            if (snake[0].style.gridColumn<9){
+                snake[0].style.gridColumn++
+            }
+            else{
+                return gameOver('You Lose')
+            }
+            break
+
+        case undefined:
+            if (snake[0].style.gridColumn>1){
+                snake[0].style.gridColumn--
+            }
+            else{
+                return gameOver('You Lose')
+            }
+            break;
+   }
+      die()
+   counterMovements.innerHTML++
+   movements.push(new Movement(snake[0].style.gridRow,snake[0].style.gridColumn))
+   if (counterMovements.innerHTML=='180'){
+       return gameOver('You Lose')
+   }
+   eatApple([snake[0].style.gridRow,snake[0].style.gridColumn],randomAppleValue)
+   moveBody()
+}
+
+function gameOver(x){        
+    let finalDiv= document.getElementById('finalDiv')
+    finalDiv.style.cssText='height:400px; width:700px;'
+    finalDiv.style.margin='0'
+    winOrlose.innerHTML=x
+    scores.style.display='block';
+    scores.children[0].innerHTML=`${20 - counter.innerHTML} Apple(s)`
+    scores.children[1].innerHTML=`${counterMovements.innerHTML} Movements`
+    winOrlose.style.display='flex';
+    playAgain.style.display='flex';
+    window.clearInterval();
+    gameOverControl=true
+}
+
+playAgain.addEventListener('click',repeatGame)
+
+function repeatGame(){
+    snake[0].style.gridRow=5;
+    snake[0].style.gridColumn=5;        
+    counter.innerHTML='20';
+    counterMovements.innerHTML='0';
+    for (let i=0; i<snakeBody.length;i++){
+    board.removeChild(snakeBody[i])
+    }
+    snakeBody=[];
+    finalDiv.style.cssText='height:0px; width:0px; margin-left:350px; margin-right:350px; margin-top:200px; margin-bottom:200px;'
+    scores.style.display='none';;
+    winOrlose.style.display='none';
+    playAgain.style.display='none';
+    movements=[{
+        row: 5,
+        column:5
+    }];
+    switchControl=undefined;
+    gameOverControl=false;
+    generateApple()
+
+}
+
+function die(){
+    for (let i=0; i<snakeBody.length;i++){
+        if (snake[0].style.gridColumn== snakeBody[i].style.gridColumn && snake[0].style.gridRow==snakeBody[i].style.gridRow){
+            return gameOver('You Lose')
+        }
     }
 }
